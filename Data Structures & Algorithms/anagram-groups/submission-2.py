1d25d2d596ce7295c73794d2998c93b969349fb9
Jian-De Jiang <jiandej@andrew.cmu.edu>
2026-04-04 23:03:49 -0400

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.py
@@ -0,0 +1,14 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        cache = {}
+        for s in strs:
+            # build map
+            count = [0]*26
+            for c in s:
+                count[ord(c) - ord('a')]+=1
+            hash_key = tuple(count)
+            if (hash_key not in cache):
+                cache[hash_key] = []
+            cache[hash_key].append(s)
+        
+        return list(cache.values())
