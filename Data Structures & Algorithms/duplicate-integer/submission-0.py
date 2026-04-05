7bb2fca10d569bc6f7a80ba2a6cc1454f650cb09
Jian-De Jiang <jiandej@andrew.cmu.edu>
2026-04-04 22:41:38 -0400

    Add: duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.py
@@ -0,0 +1,10 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        cache = set()
+
+        for n in nums:
+            if (n in cache):
+                return True
+            cache.add(n)
+        
+        return False
