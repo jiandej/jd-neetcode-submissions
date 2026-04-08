f830d6c22532a8f7e7cc79b35715297dd9284f02
Jian-De Jiang <jiandej@andrew.cmu.edu>
2026-04-08 19:42:07 -0400

    Add: course-schedule - submission-3

---
+++ Data Structures & Algorithms/course-schedule/submission-3.py
@@ -0,0 +1,25 @@
+from collections import defaultdict
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        graph = [[] for _ in range(numCourses)]
+        indegree = [0]*numCourses
+
+        for prerequisite in prerequisites:
+            graph[prerequisite[1]].append(prerequisite[0])
+            indegree[prerequisite[0]]+=1
+        
+        queue = collections.deque([i for i in range(numCourses) if indegree[i] == 0])
+        count = 0
+
+        while(queue):
+            course = queue.popleft()
+            count += 1
+
+            for next_course in graph[course]:
+                indegree[next_course] -= 1
+
+                if (indegree[next_course] == 0):
+                    queue.append(next_course)
+        
+        return count == numCourses
+        
