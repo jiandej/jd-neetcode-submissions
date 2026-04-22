a51c50bcfe9413e0b88be4359864d5a63cc98db6
Jian-De Jiang <jiandej@andrew.cmu.edu>
2026-04-21 22:40:13 -0400

    Add: subtree-of-a-binary-tree - submission-1

---
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-1.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:   
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        def serialize(node: Optional[TreeNode]):
+            if (not node):
+                return ",#"
+            
+            return f",{node.val}" + serialize(node.left) + serialize(node.right)
+        
+        root_string = serialize(root)
+        subroot_string = serialize(subRoot)
+
+        return subroot_string in root_string
